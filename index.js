2e46a2b2a6a95cef8464ca11fb63b4f5f793f416
enti <enti@enti.enti>
2022-03-04 09:08:32 +0100

    Nodemon

--- index.js
+++ index.js
@@ -1,15 +1,58 @@
 #!/usr/bin/node
 
-
-
-
 let http = require("http");
+
+let mongo_client = require("mongodb").MongoClient;
+
+let url = "mongodb://localhost/";
+
+let db;
+
+console.log("Iniciando script mongo-http");
+
+mongo_client.connect(url, function(error, conn){
+	console.log("Dentro de MongoDB");
+
+	if (error){
+		console.log("ERROR!!!");
+		return;
+	}
+
+	db = conn.db("tffhd");
+
+});
+
+
 
 http.createServer(function(req, res){
 	res.writeHead(200);
 
-	let saludo = "<h1>ola k ase</h1>";
+	if (req.url == "/"){
+		res.end();
 
-	res.end(saludo);
+		return;
+	}
+
+	let col = "";
+
+	if (req.url == "/characters")
+		col = "characters";
+	else if (req.url == "/items")
+		col = "items";
+	else{
+		res.end();
+		return;
+	}
+
+	let col_data = db.collection(col).find();
+
+	col_data.toArray(function(err, data){
+		let string = JSON.stringify(data);
+
+		res.end(string);
+	});
+
 
 }).listen(1095);
+
+
