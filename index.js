e85f32930a9690f7fa445b0b9af4ae46ec9d421c
enti <enti@enti.enti>
2022-05-15 12:21:22 +0200

    Acti Final 1

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 #!/usr/bin/node
-
-//Vars
 let http = require("http");
 let mongo_client = require("mongodb").MongoClient;
 let ObjectId = require("mongodb").ObjectID;
-
 let url = "mongodb://localhost/";
 let db;
 let fs = require("fs");
 console.log("Iniciando Script mongo_http");
 
-//Mongo Client
 mongo_client.connect(url, function(error,conn){
-	console.log("Dentro de Mongo\)");
+	console.log("Dentro de Mongo");
 	if (error){
 		console.log("ERROR");
 		return;
@@ -27,7 +23,6 @@ function send_data_list(db, req, res) {
 	else if (req.url == "/items") {col = "items";}
 	else {res.end(); return;}
 	
-	//let col_data = db.collection(col).find({},{projection: {name:1, item:1} });
 	let col_data = db.collection(col).find({});
 	
 	col_data.toArray(function(err, data){
@@ -35,13 +30,9 @@ function send_data_list(db, req, res) {
 		res.end(string);
 	});
 }
-
-//HTTP Server
 http.createServer(function(req, res) {
 	res.writeHead(200);
-
 	let url = req.url.split("/");
-
 	if (req.url == "/") {
 		fs.readFile("index.html",function(err, data){
 			res.writeHead(200, {"Content-Type":"text/html"});
@@ -49,7 +40,6 @@ http.createServer(function(req, res) {
 		});
 		return;
 	}
-	
 	if(url.length == 2) {
 		send_data_list(db, req, res);
 		return;
@@ -75,11 +65,11 @@ http.createServer(function(req, res) {
 		}
 		else if (url[1] == "removeCharacter") {
 			db.collection("characters").deleteOne({"id_character":parseInt(url[2])});
-			res.end("CHARACTER DELETED");
+					res.end("CHARACTER ELIMINADO");
 		}
 		else if (url[1] == "removeItem"){
 			db.collection("items").deleteOne({"id_item":parseInt(url[2])});
-			res.end("ITEM DELETED");
+			res.end("ITEM ELIMINADO");
 		}
 	}
 
